fa115f3dd8801e91bf947f72cb09df7a8112169e
paraschohan <paraschohan204@gmail.com>
2025-12-15 22:54:15 +0500

    push  all file

--- client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("E:\\sadhna\\pri-spec-kitplus\\.claude\\physical-ai-human-robot\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
-  require("E:\\sadhna\\pri-spec-kitplus\\.claude\\physical-ai-human-robot\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("E:\\sadhna\\pri-spec-kitplus\\.claude\\physical-ai-human-robot\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("E:\\sadhna\\pri-spec-kitplus\\.claude\\physical-ai-human-robot\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("E:\\sadhna\\pri-spec-kitplus\\.claude\\physical-ai-human-robot\\src\\css\\custom.css"),
-];
